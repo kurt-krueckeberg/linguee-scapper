881ecfad9b563b20a99ff6080a4b23902f5c42b8
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-01-17 18:05:38 -0500

    updates

--- LingueeScrapper.php
+++ LingueeScrapper.php
@@ -5,10 +5,8 @@ include 'vendor/autoload.php';
 use Einenlum\LingueeApi\Factory;
 use \SplFileObject as File;
 
-class LingueeScraper {
-
-static private $skel = <<<EOS
-<?xml version="1.0" encoding="UTF-8"?>
+/*
+ * <?xml version="1.0" encoding="UTF-8"?>
 <results>
   <L2_language>German</L2_language>
   <L1_language>English</L1_language>
@@ -35,6 +33,39 @@ static private $skel = <<<EOS
         </translation>
       </translations>
   </result>
+  </results>
+*/
+total 148
+drwxrwxr-x  5 kurt kurt      4096 Jan 17 18:01 .
+-rwxrw-r--  1 kurt kurt      3717 Jan 17 18:01 LingueeScrapper.php
+-rw-r--r--  1 kurt kurt     20480 Jan 17 18:01 .LingueeScrapper.php.swp
+-rw-rw-r--  1 kurt kurt     18907 Jan 17 17:50 new.html
+-rw-rw-r--  1 kurt kurt      1342 Jan 17 17:49 template.html
+-rw-rw-r--  1 kurt kurt       707 Jan 17 17:49 general-style.css
+drwxrwxr-x  8 kurt kurt      4096 Jan 17 15:18 .git
+-rw-rw-r--  1 kurt kurt      3000 Jan 17 15:18 xml.md
+drwxrwxrwx 11 root www-data  4096 Jan 17 12:01 ..
+-rw-rw-r--  1 kurt kurt       275 Jan 17 11:38 skeleton.html
+-rw-rw-r--  1 kurt kurt      1152 Jan 16 13:47 x.php
+-rw-rw-r--  1 kurt kurt       886 Jan 16 13:43 s.xml
+-rw-r--r--  1 root root       894 Jan 16 10:11 linguee.xml
+-rw-rw-r--  1 kurt kurt        43 Jan 13 18:23 .gitignore
+drwxrwxr-x  3 kurt kurt      4096 Jan 13 18:23 nbproject
+-rw-rw-r--  1 kurt kurt        45 Jan 13 15:22 words.txt
+lrwxrwxrwx  1 kurt kurt        19 Jan 13 15:20 l.php -> LingueeScrapper.php
+drwxrwxr-x  9 kurt kurt      4096 Jan 13 14:19 vendor
+-rw-rw-r--  1 kurt kurt     40286 Jan 13 14:19 composer.lock
+-rw-rw-r--  1 kurt kurt        68 Jan 13 14:19 composer.json
+-rw-rw-r--  1 kurt kurt       873 Jan 12 17:48 readme.md
+class LingueeScraper {
+
+static private $skel = <<<EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<results>
+  <L2_language>German</L2_language>
+  <L1_language>English</L1_language>
+  <result>
+  </result>
 </results>
 EOS;
 
@@ -45,15 +76,15 @@ EOS;
   private $word;
   private $fname;
 
+  private $cur_result;
+
   public function __construct(string $xml_name)
    {
      $this->fname = $sml_fname;	  
      $this->linguee = Factory::create(); 
 
      $this->xml = new SimpleXMLElement(self::$skel);
-
-     //$this->word = $this->xml->results->result->word; // $this->xml->xpath('/results/result/word');
-   
+     $this->cur = 0;
   }
 
   public function __destruct()
@@ -65,15 +96,15 @@ EOS;
   {
       $response = $this->linguee->translate($word, 'ger', 'eng');
 
-      $a = $response->toArray();
+      $resp = $response->toArray();
 
-      // -- $german_Word = trim($a['query'])); // TODO: This is also in the input file. So we can ignore it--righ?
+      $this->xml->results->addElement('result');
 
-      $this->word = $this->xml->xpath('/results/result/word'); //???
+      $result = $this->xml->results->result[$this->cur++];
       
-      $dict_entry->addElement($dict_entry);
+      $result->addChild( 'word', trim($resp['query']) ); 
 
-      $x = $a['words']; // $x has the translations and their assocaiated examples.
+      $resp['words']
 
       foreach($x as $y) { 
 
